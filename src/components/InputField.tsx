a171d8d98b988589f0dac8dc128a283dea8b22d0
Eric Wold <ericewold@gmail.com>
2020-12-07 11:32:26 -0700

    create reusable InputField component

---
+++ src/components/InputField.tsx
@@ -0,0 +1,30 @@
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage
+} from '@chakra-ui/react';
+import React, { InputHTMLAttributes } from 'react';
+import { useField } from 'formik';
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  label: string;
+  placeholder: string;
+  name: string;
+};
+
+export const InputField: React.FC<InputFieldProps> = props => {
+  const [field, { error }] = useField(props);
+
+  // !!error
+  // '' = true
+  // 'stuff' = false
+
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor={field.name}>{props.label}</FormLabel>
+      <Input {...field} id={field.name} placeholder={props.placeholder} />
+      {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+    </FormControl>
+  );
+};
