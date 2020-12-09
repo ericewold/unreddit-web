b383db171158879b4b608dc07bc099e736a01f12
Eric Wold <ericewold@gmail.com>
2020-12-09 10:14:53 -0700

    use withUrqlClient, display post objects on index

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,18 +1,14 @@
 import React from "react";
-import { Formik, Form } from "formik";
-import {
-  FormControl,
-  FormLabel,
-  Input,
-  FormErrorMessage,
-  Box,
-  Button
-} from "@chakra-ui/react";
+import { Form, Formik } from "formik";
+import { Box, Button } from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
 import { useRegisterMutation } from "../generated/graphql";
 import { toErrorMap } from "../utils/toErrorMap";
 import { useRouter } from "next/router";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrqlClient";
+
 
 interface registerProps {}
 
@@ -64,4 +60,4 @@ const Register: React.FC<registerProps> = ({}) => {
   );
 };
 
-export default Register;
+export default withUrqlClient(createUrqlClient)(Register);
