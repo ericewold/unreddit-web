91d755f20b24e99b5ee49ed3101d9014c6dd53e0
Eric Wold <ericewold@gmail.com>
2020-12-08 21:08:07 -0700

    NavBar is working now. TODO: update cache after user login/register. Currently need to refresh page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Box, Flex, Link } from '@chakra-ui/react';
+import { Box, Button, Flex, Link } from '@chakra-ui/react';
 import NextLink from 'next/link';
+import { useMeQuery } from '../generated/graphql';
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
-  return (
-    <Flex bg='tomato' padding={4}>
-      <Box ml={'auto'}>
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+
+  // data is loading
+  if (fetching) {
+    // user not logged in
+  } else if (!data?.me) {
+    body = (
+      <>
         <NextLink href='/login'>
           <Link color='white' fontWeight='bold' mr={4}>
             login
@@ -18,7 +25,20 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
             register
           </Link>
         </NextLink>
-      </Box>
+      </>
+    );
+    // user is logged in
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant='link'>logout</Button>
+      </Flex>
+    );
+  }
+  return (
+    <Flex bg='tomato' padding={4}>
+      <Box ml={'auto'}>{body}</Box>
     </Flex>
   );
 };
