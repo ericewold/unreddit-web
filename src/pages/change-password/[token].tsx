5cdfc6c4cd67b7d48cb5a4f4b9e712a23a36363e
Eric Wold <ericewold@gmail.com>
2020-12-09 20:28:22 -0700

    building change-password feature. added [token] for nextjs

---
+++ src/pages/change-password/[token].tsx
@@ -0,0 +1,55 @@
+import { Box, Button } from "@chakra-ui/react";
+import { Formik, Form } from "formik";
+import { NextPage } from "next";
+import React from "react";
+import { InputField } from "../../components/InputField";
+import { Wrapper } from "../../components/Wrapper";
+import { toErrorMap } from "../../utils/toErrorMap";
+import login from "../login";
+
+export const ChangePassword: NextPage<{ token: string }> = ({ token }) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ newPassword: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          // const response = await login(values);
+          // if (response.data?.login.errors) {
+          //   setErrors(toErrorMap(response.data.login.errors));
+          // } else if (response.data?.login.user) {
+          //   // login worked
+          //   router.push("/");
+          // }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="newPassword"
+              placeholder="new password"
+              label="New Password"
+              type="password"
+            />
+
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              colorScheme="teal"
+            >
+              Change Password
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+ChangePassword.getInitialProps = ({ query }) => {
+  return {
+    token: query.token as string,
+  };
+};
+
+export default ChangePassword;
