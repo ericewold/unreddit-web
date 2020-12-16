5e2a635e1db6469e4972ede3062c2b013b2de81f
Eric Wold <ericewold@gmail.com>
2020-12-15 21:11:44 -0700

    user lands on create-post > redirect to /login if necessary

--- src/pages/create-post.tsx
+++ src/pages/create-post.tsx
@@ -2,15 +2,20 @@ import { Box, Button } from "@chakra-ui/react";
 import { Formik, Form } from "formik";
 import { withUrqlClient } from "next-urql";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect } from "react";
 import { InputField } from "../components/InputField";
 import { Layout } from "../components/Layout";
-import { useCreatePostMutation } from "../generated/graphql";
+import { useCreatePostMutation, useMeQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 
-
 const CreatePost: React.FC<{}> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
   const router = useRouter();
+  useEffect(() => {
+    if (!fetching && !data?.me) {
+      router.replace("/login");
+    }
+  });
   const [, createPost] = useCreatePostMutation();
   return (
     <Layout variant="small">
