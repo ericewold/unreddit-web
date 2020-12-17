67e05f501a8e0382811f855f657edb87d7aacdf6
Eric Wold <ericewold@gmail.com>
2020-12-17 13:04:52 -0700

    import css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 import theme from "../theme";
+import "../style/main.css";
 
 function MyApp({ Component, pageProps }: any) {
   return (
     <ChakraProvider resetCSS theme={theme}>
       <ColorModeProvider
         options={{
-          useSystemColorMode: true
+          useSystemColorMode: true,
         }}
       >
         <Component {...pageProps} />
