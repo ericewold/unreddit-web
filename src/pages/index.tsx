603d74272d8c07662bb829e61bfa0d6c527e84de
Eric Wold <ericewold@gmail.com>
2020-12-18 19:32:09 -0700

    add post link to index list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,7 +49,11 @@ const Index = () => {
             <Flex key={p.id} p={5} shadow="md" borderWidth="1px">
               <UpvoteSection post={p} />
               <Box>
-                <Heading fontSize="xl">{p.title}</Heading>
+                <NextLink href="/post/[id]" as={`/post/${p.id}`}>
+                  <Link>
+                    <Heading fontSize="xl">{p.title}</Heading>
+                  </Link>
+                </NextLink>
                 <Text color="#cccccc">{p.creator.username}</Text>
                 <Text mt={4}>{p.textSnippet}</Text>
               </Box>
