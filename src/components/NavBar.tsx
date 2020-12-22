9ff3f35d61c5e0e8100bc0196957b0715f8a584a
Eric Wold <ericewold@gmail.com>
2020-12-21 21:46:46 -0700

    login & logout now call invalidateAllPosts() to smoothly update the cache for posts & votes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,10 +3,12 @@ import { Box, Button, Flex, Link } from "@chakra-ui/react";
 import NextLink from "next/link";
 import { useLogoutMutation, useMeQuery } from "../generated/graphql";
 import { isServer } from "../utils/isServer";
+import { useRouter } from "next/router";
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const router = useRouter();
   const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
   const [{ data, fetching }] = useMeQuery({
     pause: isServer(),
@@ -57,8 +59,9 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
             {data.me.username}
           </Flex>
           <Button
-            onClick={() => {
-              logout();
+            onClick={async () => {
+              await logout();
+              router.reload();
             }}
             isLoading={logoutFetching}
             variant="link"
